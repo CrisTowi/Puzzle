2a9670745e532c6d55a58a0e50ecb31e82de3a78
Christian Consuelo <christian_consuelo@outlook.com>
2013-09-17 10:05:34 -0500

    Initial Commit
    El avance que llevamos =)

---
+++ puz/src/puz/Puz.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package puz;
+
+/**
+ *
+ * @author Anemone
+ */
+public class Puz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Panel obj = new Panel();
+        obj.setVisible(true);
+        obj.setLocationRelativeTo(null);
+    }
+}
